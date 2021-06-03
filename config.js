f79bd773ce18a99562799246515a0b6d64d02899
ducdx2002 <85302281+ducdx2002@users.noreply.github.com>
2021-06-04 00:10:28 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Lợn à!',
+    introDesc: `Ủa lại bị Đức lừa chui vô đây chứ gì :))`,
+    btnIntro: 'Ấn thử xem',
+    title: ' Có lẽ là anh nên nói: ',
+    desc: ' Một vài điều từ đôi môi, chỉ cần em thôi ',
+    btnYes: ' Yêu thế <3',
+    btnNo: ' Cần db cc -.-',
+    question:'Why ấn vô đây haha',
+    btnReply: 'Gửi <3',
+    reply: ' Viết gì cũng ra Đức iu Nhung thôi :)))',
+    mess: ' Bất ngờ thế :))',
+    messDesc: ' Thực ra m có ấn vào đây t cũng k biết đâu hehe!!!.',
+    btnAccept: ':)))',
+    messLink: 'http://fb.com/dxduc02'
+}
